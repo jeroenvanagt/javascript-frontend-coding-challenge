58a62bc864c2acf2f45bcae18fd3db649fabac26
Jeroen van Agt <jeroen@olino.org>
2019-06-06 10:19:41 +0200

    Removed debug logging

--- ApiHandler.js
+++ ApiHandler.js
@@ -7,7 +7,6 @@ export default class ApiHandler{
 
     getData(query){
         let url = stringInject(this.options.url,{query: query, count: this.options.numOfResults});
-        console.log(url);
         return fetch(url)
         .then( resp => resp.json())
         .then( data =>  {
